216a7715ca966f8ae78f2cbba626a745a6d11402
yudiuth87 <yudiutama196@gmail.com>
2025-08-22 22:47:06 +0800

    revisi alam / perubahan narasi

--- resources/views/offset.blade.php
+++ resources/views/offset.blade.php
@@ -487,10 +487,7 @@
   </div>
 
   <!-- Search -->
-  <div style="margin-bottom:24px;">
-    <input type="text" id="searchLokasi" placeholder="Cari Lokasi Penanaman"
-      style="width:100%;padding:12px 16px;border-radius:10px;border:1px solid #ddd;font-size:15px;">
-  </div>
+
 
   <!-- Location Cards -->
   <div id="lokasiCards"></div>
